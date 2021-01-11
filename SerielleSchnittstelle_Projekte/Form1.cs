156f7c36e5a4c72c9529176a0b50f22879ea6049
Marc-Rafael <mr.schulte@outlook.com>
2021-01-11 18:08:28 +0100

    h

--- SerielleSchnittstelle_Projekte/Form1.cs
+++ SerielleSchnittstelle_Projekte/Form1.cs
@@ -19,7 +19,7 @@ namespace SerielleSchnittstelle_Projekte
 
         private void button1_Click(object sender, EventArgs e)
         {
-            button1.Text = "Hal";
+            button1.Text = "Halll";
         }
     }
 }
